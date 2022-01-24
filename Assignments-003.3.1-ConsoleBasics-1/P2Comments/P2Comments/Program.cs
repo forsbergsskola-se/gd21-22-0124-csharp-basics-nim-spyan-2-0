9e803dcda5bc14daf73abbeeb936b83b91d3ab58
SearWaizy <sear.waizy@student.forsbergsskola.se>
2022-01-24 15:17:05 +0100

    P2 Comments :)

---
+++ Assignments-003.3.1-ConsoleBasics-1/P2Comments/P2Comments/Program.cs
@@ -0,0 +1,14 @@
+﻿//Sear Waizy & Elias Bernmarker
+//Date 2022-01-24
+//sear.waizy@skola.forsbergsskola.se & elias.bernmarker@student.forsbergsskola.se
+public class program
+{
+    /// <summary>
+    /// The Main method is the entry point of a C# application
+    /// </summary>
+    public static void Main()
+    {
+        //We access the console, then we write the line "HelloWorld" which will output into the console.
+        Console.WriteLine("HelloWorld");
+    }
+}
