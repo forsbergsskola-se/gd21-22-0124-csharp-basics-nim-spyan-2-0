43ecc148747c17de219c215baef3c8941787cf27
Elias Bernmarker <elias.bernmarker@hotmail.com>
2022-01-25 13:55:43 +0100

    P5 Template

---
+++ Assignments-003.3.1-ConsoleBasics-1/P5Conversion/P5Conversion/Program.cs
@@ -0,0 +1,10 @@
+﻿int number = 5;
+string numberString = Convert.ToString(number);
+
+Console.WriteLine(numberString);
+
+numberString = "56.3";
+
+number = Convert.ToInt32(numberString);
+
+Console.WriteLine(number);
