2f62abf8759c1c93b8f6fc4df19b8cedb56aa699
Elias Bernmarker <elias.bernmarker@hotmail.com>
2022-01-25 14:28:18 +0100

    P5

--- Assignments-003.3.1-ConsoleBasics-1/P5Conversion/P5Conversion/Program.cs
+++ Assignments-003.3.1-ConsoleBasics-1/P5Conversion/P5Conversion/Program.cs
@@ -1,10 +1,13 @@
 ﻿int number = 5;
+float floatToInt;
 string numberString = Convert.ToString(number);
 
 Console.WriteLine(numberString);
 
-numberString = "56.3";
+numberString = "56,3";
 
-number = Convert.ToInt32(numberString);
+floatToInt = Convert.ToSingle(numberString);
+
+number = (int)Math.Round(floatToInt);
 
 Console.WriteLine(number);
