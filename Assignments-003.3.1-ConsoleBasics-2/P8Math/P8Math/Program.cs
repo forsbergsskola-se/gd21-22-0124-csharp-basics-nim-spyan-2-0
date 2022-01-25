f89f772b7e88e62ee6991005bda9ac235c5e60eb
Elias Bernmarker <elias.bernmarker@hotmail.com>
2022-01-25 16:02:20 +0100

    P8 Math

--- Assignments-003.3.1-ConsoleBasics-2/P8Math/P8Math/Program.cs
+++ Assignments-003.3.1-ConsoleBasics-2/P8Math/P8Math/Program.cs
@@ -1,3 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿Console.WriteLine("Give me your first number.");
 
-Console.WriteLine("Hello, World!");
+string numOne = Console.ReadLine();
+double first  = Convert.ToDouble(numOne);
+
+Console.WriteLine("Give me your second number.");
+
+string numTwo = Console.ReadLine();
+double second = Convert.ToDouble(numTwo);
+
+Console.WriteLine("Max:");
+Console.WriteLine(Math.Max(first, second));
+Console.WriteLine("Min:");
+Console.WriteLine(Math.Min(first, second));
+Console.WriteLine("Sqrt:");
+Console.WriteLine(Math.Sqrt(first));
+Console.WriteLine("Abs:");
+Console.WriteLine(Math.Abs(first));
+Console.WriteLine("Round:");
+Console.WriteLine(Math.Round(first));
+Console.WriteLine("Floor:");
+Console.WriteLine(Math.Floor(first));
+Console.WriteLine("Ceil:");
+Console.WriteLine(Math.Ceiling(first));
+Console.WriteLine("Clamp:");
+Console.WriteLine(Math.Clamp(first, 0, second));
+Console.WriteLine("Pow:");
+Console.WriteLine(Math.Pow(first, second));
