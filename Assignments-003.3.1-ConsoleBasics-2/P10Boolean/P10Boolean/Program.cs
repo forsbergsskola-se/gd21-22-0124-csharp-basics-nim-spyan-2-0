c4e8a696a0f363400c369ff6bccff0018a6432c0
Elias Bernmarker <elias.bernmarker@hotmail.com>
2022-01-27 14:14:37 +0100

    Better bool script

--- Assignments-003.3.1-ConsoleBasics-2/P10Boolean/P10Boolean/Program.cs
+++ Assignments-003.3.1-ConsoleBasics-2/P10Boolean/P10Boolean/Program.cs
@@ -5,20 +5,9 @@ bool isGrownup = false;
 Console.WriteLine("What's your age?");
 int age = Convert.ToInt32(Console.ReadLine());
 
-if (age >= 0 && age <= 12)
-{
-    isChild = true;
-}
-
-if (age >= 13 && age <= 19)
-{
-    isTeenager = true;
-}
-
-if (age > 19)
-{
-    isGrownup = true;
-}
+isChild = age >= 0 && age <= 12;
+isTeenager = age >= 13 && age <= 19;
+isGrownup = age > 19;
     
 Console.WriteLine(isChild);
 Console.WriteLine(isTeenager);
