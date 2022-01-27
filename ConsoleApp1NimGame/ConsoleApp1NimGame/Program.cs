317b18065a79d50c5b947af67b24a3701f2d9ef6
Elias Bernmarker <elias.bernmarker@hotmail.com>
2022-01-27 18:52:52 +0100

    Nim update

--- ConsoleApp1NimGame/ConsoleApp1NimGame/Program.cs
+++ ConsoleApp1NimGame/ConsoleApp1NimGame/Program.cs
@@ -1,3 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿int Match = 24;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Choose to pick 1, 2, or 3 matches! The player that picks the last match looses.");
+
+while (Match > 1)
+{
+    Console.WriteLine("Do you want to pick 1, 2, or 3 Matches?");
+    
+    int choice = Convert.ToInt32(Console.ReadLine());
+    if (choice == 1)
+    {
+        Match--;
+    }
+    else if (choice == 2)
+    {
+        Match--;
+        Match--;
+    }
+    else if (choice == 3)
+    {
+        Match--;
+        Match--;
+        Match--;
+    }
+    else
+    {
+        Console.WriteLine("You can't do that!");
+    }
+    Console.WriteLine(Match);
+}
